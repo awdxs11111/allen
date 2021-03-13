aeddad1d3cb1c63f48e731a661a57fcb6f1c00af
allenqq1209 <77651462+allenqq1209@users.noreply.github.com>
2021-03-13 23:11:51 +0800

    Update wkzz.js

--- task/wkzz.js
+++ task/wkzz.js
@@ -181,7 +181,9 @@ tid =data.match(/"a_id":(.*?),/)[1]
 name =data.match(/"content_url":"(.*?)",/)[1]
 
         console.log('\n微客众智获取任务ID成功\n当前任务ID: '+uid+' '+tid+'\n开始循环阅读:')
-        await $.wait(1000);
+        random = Math.floor(Math.random()*(max-min+1)+min)*1000
+        console.log("随机延时"+random+"毫秒");
+        await $.wait(random); 
         await wkzzyd();
 } else {
        console.log('\n微客众智获取任务ID失败  '+result.data.message)
@@ -211,9 +213,7 @@ let url = {
             $.logErr(`API请求失败，请检查网络后重试 \n data: ${data}`)
           } else {
 console.log('\n微客众智阅读文章成功,开始领取阅读奖励')
-        random = Math.floor(Math.random()*(max-min+1)+min)*1000
-        console.log("随机延时"+random+"毫秒");
-	      await $.wait(random); 
+        await $.wait(1000);
         await wkzzyd();
 } 
    
